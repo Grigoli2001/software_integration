5c26a565f950af3815e34d7a090407e5f351605f
Grigoli2001 <gpatsatsiausa@gmail.com>
2024-08-01 11:41:40 +0200

    chore: Update logger message in startApp function

--- src/boot/index.ts
+++ src/boot/index.ts
@@ -73,7 +73,7 @@ export const startApp = (): void => {
     registerCoreMiddleWare();
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
-      logger.info(`Server is running on port ${PORT}`);
+      logger.info(`Server is running on port ${PORT}!`);
     });
     handleErrors();
   } catch (error) {
